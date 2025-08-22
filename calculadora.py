032dfe6a31c2fecaeb92018414e4637a8120cbd5
IvoSobral <ivo.sobral@hotmail.com>
2025-08-22 16:25:09 -0300

    upload

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+
+def soma(a,b):
+   return a + b
+
+def subtrair(c,d):
+   return c - d
+
+def multiplicar(e,f):
+   return e * f
+
+def dividir (g,h):
+    if h == 0:
+        return None
+    return g/h
+
+def raiz_quadrada(i):
+   return i ** 0.5
+
+def divisao_inteiro(j,k):
+    if k == 0:
+        return None
+    return j//k
